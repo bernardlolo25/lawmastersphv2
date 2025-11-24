345cda31f0eef1e54be3d105c6f0e13bd29784e8
bernardlolo25 <73865640+bernardlolo25@users.noreply.github.com>
2025-11-24 14:35:44 +0800

    Add files via upload
    helloo

---
+++ next.config.ts
@@ -0,0 +1,60 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sl.bing.net',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'tse3.mm.bing.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uspf-program.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+   /**
+   * NOTE: This is a temporary workaround for a known issue with Next.js and WebContainers.
+   * In a real-world scenario, you would want to be more specific with the allowed origins.
+   * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
+   */
+  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+};
+
+export default nextConfig;
